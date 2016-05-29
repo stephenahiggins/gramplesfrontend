ab70c5472193790553b078bb838bdb140c039c80
Stephen Higgins <stephenahiggins@icloud.com>
2016-05-29 16:47:43 +0100

    Fix image display

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -42,39 +42,64 @@ angular.module('starter.controllers', [])
     }
 }) 
 
-.controller('takePhoto', function($scope, $cordovaCamera){
-     $scope.takePhoto = function() {
-       var options = {
-       fileKey: "avatar",
-       fileName: "image.png",
-       chunkedMode: "false",
-       mimeType: "false",
-       quality: 75,
-       destinationType: Camera.DestinationType.DATA_URL,
-       sourceType: Camera.PictureSourceType.CAMERA,
-       allowEdit: true,
-       encodingType: Camera.EncodingType.JPEG,
-       targetWidth: 300,
-       targetHeight: 300,
-       popoverOptions: CameraPopoverOptions,
-       saveToPhotoAlbum: false
-   };
-    $cordovaCamera.getPicture(options).then(function(imageData) {
-      $scope.imgURI = "data:image/jpeg;base64," + imageData;
-      formData.img = $scope.imgURI; 
-      console.log(formData); 
-       console.log($scope.formData); 
-          }, function(err) {
-         // An error occured. Show a message to the user 
-        }); 
-     };
+.controller('getImage', function($scope, $cordovaCamera, $cordovaImagePicker) {
+  // Camera Functions 
+    $scope.takePhoto = function() {
+      var options = {
+      fileKey: "avatar",
+      fileName: "image.png",
+      chunkedMode: "false",
+      mimeType: "false",
+      quality: 75,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 300,
+      targetHeight: 300,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false
+  };
+
+   $cordovaCamera.getPicture(options).then(function(imageData) {
+     $scope.imgURI = "data:image/jpeg;base64," + imageData;
+         }, function(err) {
+        // An error occured. Show a message to the user 
+       }); 
+    };
+
+    // End Camera Functions 
+
+    // Gallery Functions 
+    $scope.getImageFromGalleryClick = function() {
+      var options = {
+       maximumImagesCount: 1,
+       width: 300,
+       height: 300,
+       quality: 80
+      };
+
+    $cordovaImagePicker.getPictures(options).then(function (results) {
+           for (var i = 0; i < results.length; i++) {
+               console.log('Image URI: ' + results[i]); 
+              // Encode URI to Base64
+              window.plugins.Base64.encodeFile(results[i], function(base64){
+                 // Save images in Base64
+                $scope.imgURI = base64; 
+                $scope.$apply()
+              });
+            }
+        }, function(error) {
+        // error getting photos
+      });
+   }
+    // End Gallery Functions 
 })
 
 .controller("uploadController", function($scope, $cordovaFileTransfer) {
- 
-    $scope.upload = function() {
-      console.log('Wag:', formData); 
-      /*
+    $scope.uploadClick = function() {
+      console.log('Wag:'); 
+    
         var options = {
             fileKey: "avatar",
             fileName: "image.png",
@@ -82,12 +107,12 @@ angular.module('starter.controllers', [])
             mimeType: "image/png"
         };
         $cordovaFileTransfer.upload("http://192.168.56.1:1337/file/upload", "/android_asset/www/img/ionic.png", options).then(function(result) {
-            console.log("SUCCESS: " + JSON.stringify(result.response));
+            console.log("SUC CESS: " + JSON.stringify(result.response));
         }, function(err) {
             console.log("ERROR: " + JSON.stringify(err));
         }, function (progress) {
             // constant progress updates
         });
-        */
+      
     }
 });
